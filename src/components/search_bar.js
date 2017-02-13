ffca55db51fed625d28c680182085884d154c542
Tom Scott <tomscott85@gmail.com>
2017-02-13 22:19:03 +1100

    update search bar to have state

---
+++ src/components/search_bar.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { term: '' }
+  }
+
+  render() {
+    return (
+      <div>
+        <input onChange={ event => this.setState({ term: event.target.value })} />
+        Value of the input: {this.state.term}
+      </div>
+    )
+  }
+
+  // onInputChange(event) {
+  //   console.log(event.target.value)
+  // }
+
+}
+
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import youtubeSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyClsGj7v1_sBcypLIgZUtHkwr9SmWVEPT8';
 
 // Create a new componenet. This comonent should produce some HTML
 const App = () => {
-  return <div> Hello </div>;
+  return (
+    <div>
+    <SearchBar />
+   </div>
+ );
 };
 // Take this component's generated HTML and put it on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
