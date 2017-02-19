e8f5bfb3593259eb94ebd0483a12f2caddcb84ad
Tom Scott <tomscott85@gmail.com>
2017-02-19 13:38:07 +1100

    stashing after lecture 29

--- src/index.js
+++ src/index.js
@@ -2,18 +2,24 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyClsGj7v1_sBcypLIgZUtHkwr9SmWVEPT8';
 
-
-
-// Create a new componenet. This comonent should produce some HTML
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
-    YTSearch({key: API_KEY, term: 'surfboards'}, (data) => {
-      this.setState({ videos: data })
+    this.state = {
+      videos: [],
+      selectedVideo: null
+     };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+       })
     });
   }
 
@@ -21,6 +27,11 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
+          videos={this.state.videos}
+        />
       </div>
    );
   }
